76eafdd7d07369962039b965d43f8dcefe5b12f4
Tamara MT <tamaramolinast@gmail.com>
2023-11-09 12:17:47 -0300

    added schema and moved router to products.url

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from product import views
-
-
-router = DefaultRouter()
-router.register(r"category", views.CategoryViewSet)
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
+    path("", include("product.urls")),
 ]
