28d5e4d8cfa232ac088008a51103fca87672a53c
Tamara MT <tamaramolinast@gmail.com>
2023-11-09 07:26:53 -0300

    created for Brand, Category and product

---
+++ app/product/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from .models import Brand, Category, Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+        read_only_fields = ["id"]
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ["name"]
+        read_only_fields = ["id"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["name", "description", "is_digital", "brand", "category"]
+        read_only_fields = ["id"]
