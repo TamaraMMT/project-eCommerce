9c33843528f6235b9de682efadcae768c9e97f26
Tamara MT <tamaramolinast@gmail.com>
2023-11-09 12:15:54 -0300

    implementaded docs schemas

--- app/core/settings/base.py
+++ app/core/settings/base.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "mptt",
     "product",
 ]
@@ -98,4 +99,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "DRF Ecommerce",
+}
