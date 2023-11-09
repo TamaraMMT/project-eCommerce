e93222e5d3ad948e5add69be1962e63f14522a72
Tamara MT <tamaramolinast@gmail.com>
2023-11-09 12:54:56 -0300

    added brand and product views

--- app/product/views.py
+++ app/product/views.py
@@ -1,10 +1,24 @@
 from rest_framework import viewsets
-from product.models import Category
-from product.serializers import CategorySerializer
+from product.models import Category, Brand, Product
+from product import serializers
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     """Category list"""
 
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    serializer_class = serializers.CategorySerializer
+
+
+class BrandViewSet(viewsets.ReadOnlyModelViewSet):
+    """Brand list"""
+
+    queryset = Brand.objects.all()
+    serializer_class = serializers.BrandSerializer
+
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    """Product list"""
+
+    queryset = Product.objects.all()
+    serializer_class = serializers.ProductSerializer
