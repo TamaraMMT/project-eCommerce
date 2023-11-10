f288eaaf27fd90987ac1d2946b42df748165e612
Tamara MT <tamaramolinast@gmail.com>
2023-11-10 11:11:37 -0300

    .

--- app/product/admin.py
+++ app/product/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Brand, Category, Product
+from product import models
 
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Brand)
+admin.site.register(models.Product)
+admin.site.register(models.Category)
+admin.site.register(models.Brand)
