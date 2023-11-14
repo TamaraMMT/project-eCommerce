bcfca429f3d1f1a271b353f1fc78a278ffbb7265
Tamara MT <tamaramolinast@gmail.com>
2023-11-14 19:31:13 -0300

    add is_active and related_name in product

--- app/product/models.py
+++ app/product/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from mptt.models import MPTTModel, TreeForeignKey
+from utils.fields import OrderField
 
 
 class Category(MPTTModel):
     name = models.CharField(max_length=40, unique=True)
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+    is_active = models.BooleanField(default=False)
 
     class MPTTMeta:
         order_insertion_by = ["name"]
@@ -15,6 +18,7 @@ class Category(MPTTModel):
 
 class Brand(models.Model):
     name = models.CharField(max_length=40)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -22,6 +26,7 @@ class Brand(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255)
     description = models.TextField(max_length=200)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
@@ -38,5 +43,7 @@ class ProductLine(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=5)
     sku = models.CharField(max_length=100)
     stock_qty = models.IntegerField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_line"
+    )
     is_active = models.BooleanField(default=False)
