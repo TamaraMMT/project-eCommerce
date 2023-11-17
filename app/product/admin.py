23709c32982c20dff8fd7e91d0f7105e7f39aa39
Tamara MT <tamaramolinast@gmail.com>
2023-11-16 21:37:56 -0300

    added buttom edit

--- app/product/admin.py
+++ app/product/admin.py
@@ -1,17 +1,41 @@
 from django.contrib import admin
-
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from product import models
 
 
-class ProductLineinLine(admin.TabularInline):
+class EditLinkInLine:
+    def edit(self, instance):
+        url = reverse(
+            f"admin:{instance._meta.app_label}_{instance._meta.model_name}_change",
+            args=[instance.pk],
+        )
+        if instance.pk:
+            link = mark_safe(f'<a href="{url}">edit</a>'.format(u=url))
+            return link
+        return ""
+
+
+class ProductImageInLine(admin.TabularInline):
+    model = models.ProductImage
+
+
+class ProductLineInLine(EditLinkInLine, admin.TabularInline):
     model = models.ProductLine
+    readonly_fields = ("edit",)
 
 
-@admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ProductLineinLine]
+    inlines = [
+        ProductLineInLine,
+    ]
 
 
+class ProductLineAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInLine]
+
+
+admin.site.register(models.ProductLine, ProductLineAdmin)
+admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.Category)
 admin.site.register(models.Brand)
-admin.site.register(models.ProductLine)
