c92e50498f44c3d04eb8c452ea87312332575135
Tamara MT <tamaramolinast@gmail.com>
2023-11-16 21:41:58 -0300

    added extend_schema_view and update queryset

--- app/product/views.py
+++ app/product/views.py
@@ -1,3 +1,11 @@
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
+from django.db.models import Prefetch
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from product.models import Category, Brand, Product
@@ -20,12 +28,35 @@ class BrandViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.BrandSerializer
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "category__name",
+                OpenApiTypes.STR,
+                description="Input the category name to filter",
+            ),
+            OpenApiParameter(
+                "brand__name",
+                OpenApiTypes.STR,
+                description="Input the brand name to filter",
+            ),
+        ]
+    )
+)
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
-    """Product list"""
+    """Products"""
 
     lookup_field = "name"
-    queryset = Product.objects.filter(is_active=True)
     serializer_class = serializers.ProductSerializer
-
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["category__name", "brand__name"]
+
+    def get_queryset(self):
+        return (
+            Product.objects.filter(is_active=True)
+            .select_related("category", "brand")
+            .prefetch_related(
+                Prefetch("product_line__product_image"),
+            )
+        )
